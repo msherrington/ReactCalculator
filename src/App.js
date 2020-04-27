6036cbfb78cee0dae463a82156e259ac44e74e85
MS <msherr04@mail.bbk.ac.uk>
2020-04-27 22:18:54 +0100

    clean out old comments and redundant code

--- src/App.js
+++ src/App.js
@@ -1,62 +1,12 @@
-import React, { Component } from 'react'
-// import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 import Calculate from "./Calculator"
-
-import { Button } from 'reactstrap'
-import NumberButton from "./NumberButton"
-// import FuncComponent from './Functional.js'
-// import ParentComponent from "./ParentComponent"
-
-// class Hello extends Component {
-//   render() {
-//     return <div>Hello {this.props.toWhat}</div>
-//   }
-// }
-//
-// const buttonStyle = {
-//   margin: '10px 10px 10px 0'
-// };
-
-
-// class NumberButton extends Component {
-//
-//   render() {
-//     return <Button
-//         size="Lg"
-//         style={buttonStyle}
-//         variant="light"
-//         onClick={this.props.numberClick}>{this.props.number}</Button>
-//   }
-// }
-//
-// class OperatorButton extends Component {
-//   render() {
-//     return <Button size="Lg" variant="light">{this.props.operator}</Button>
-//   }
-// }
-
-// class NumBtn extends Component {
-//   render() {
-//     return (
-//         <button
-//             className="btn btn-default"
-//             style={buttonStyle}
-//             onClick={this.props.handleClick}>{this.props.number}</button>
-//     );
-//   }
-// }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*<Hello toWhat="Finley"/>*/}
-        {/*<FuncComponent/>*/}
-        {/*<ParentComponent/>*/}
         <Calculate/>
-
       </header>
     </div>
   );
--- src/NumberButton.js
+++ src/NumberButton.js
@@ -2,29 +2,10 @@ import React, { Component } from 'react'
 import {Button} from "reactstrap";
 
 class NumberButton extends Component {
-    constructor(props) {
-        super(props);
-
-        // This binding is necessary to make `this` work in the callback
-        this.numberClick = this.numberClick.bind(this);
-    }
-
-    numberClick() {
-        console.log('aaa', this);
-        if (!this.props.state.operator) {
-            let newValue = this.props.state.first + this.props.number;
-            this.setState(state => ({
-                first: newValue,
-                display: newValue
-            }));
-        }
-    }
-
     render() {
         return <Button
             size="Lg"
-            variant="light"
-            onClick={this.numberClick}>{this.props.number}</Button>
+            variant="light">{this.props.number}</Button>
     }
 }
 
